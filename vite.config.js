ef9bc41d6fc852294b95391f3089833e1714df74
Tuco2006 <mateussmcruz@icloud.com>
2025-10-21 15:11:03 -0300

    kenfvonefv

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Substitua "usuario" e "repositorio" pelos seus reais
 export default defineConfig({
   plugins: [react()],
-  base: '/MachinePay.github.io/', 
+  base: '/', // 👈 isso é o mais importante
 })
