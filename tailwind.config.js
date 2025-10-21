86596a5c29184841e88cf92d3a9ad799e0879a95
Tuco2006 <mateussmcruz@icloud.com>
2025-10-21 13:07:38 -0300

    tail

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
-// tailwind.config.js
-
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  // ...
-}
+  /** @type {import('tailwindcss').Config} */
+    module.exports = {
+      content: [
+        "./index.html",
+        // CRUCIAL: Deve incluir todos os arquivos de componente na sua pasta src
+        "./src/**/*.{js,ts,jsx,tsx}", 
+      ],
+      theme: {
+        extend: {},
+      },
+      plugins: [],
+    }
+    
