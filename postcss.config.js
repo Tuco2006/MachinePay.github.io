cfc1c39a6cba41878144c1984e9b39d69a7eafb6
Tuco2006 <mateussmcruz@icloud.com>
2025-10-21 10:40:18 -0300

    post

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,11 @@
-// postcss.config.js (Formato ES Module para projetos type: "module")
+// postcss.config.js (Formato de Array para plugins)
+
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    tailwindcss(), // Chama o plugin como função
+    autoprefixer(), // Chama o plugin como função
+  ],
 }
