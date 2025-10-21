23321bbccff4cc8da6ce26d67484c5587b8442dc
Tuco2006 <mateussmcruz@icloud.com>
2025-10-21 10:23:37 -0300

    correcaofinalvite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // Corrigido a importação do tailwindcss
+// ...
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // ADICIONE ESTA LINHA: O nome do seu repositório deve ser a base.
-  base: "/MachinePay.github.io/", 
   
+  base: "./", 
+
   plugins: [react(), tailwindcss()],
 })
