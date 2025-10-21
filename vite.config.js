8e277898add06f2cdbf8ad983929382d4edafde8
Tuco2006 <mateussmcruz@icloud.com>
2025-10-21 10:25:15 -0300

    tailwindcorrection

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 // vite.config.js
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// ...
+
+// NÃO PRECISA IMPORTAR NADA RELACIONADO AO TAILWIND AQUI
 
 export default defineConfig({
-  
+  // Mantenha a base para o deploy:
   base: "./", 
-
-  plugins: [react(), tailwindcss()],
+  
+  plugins: [
+    react() // Apenas o plugin React
+  ],
 })
