5ec0a409f5a4e5581187dd5c2125b3e11b415b88
Tuco2006 <mateussmcruz@icloud.com>
2025-10-21 13:15:35 -0300

    tail2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
-  /** @type {import('tailwindcss').Config} */
-    module.exports = {
-      content: [
-        "./index.html",
-        // CRUCIAL: Deve incluir todos os arquivos de componente na sua pasta src
-        "./src/**/*.{js,ts,jsx,tsx}", 
-      ],
-      theme: {
-        extend: {},
-      },
-      plugins: [],
-    }
-    
+/** @type {import('tailwindcss').Config} */
+export default { // MUDANÇA: de module.exports para export default
+  content: [
+    "./index.html",
+    // Esta linha estava com aspas simples nos arquivos que você mandou; 
+    // Garanta que o caminho de busca esteja correto:
+    "./src/**/*.{js,ts,jsx,tsx}", 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
