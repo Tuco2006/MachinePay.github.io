881558cbc3577a225703065ffc8de9ca909180be
Tuco2006 <mateussmcruz@icloud.com>
2025-10-21 12:04:19 -0300

    slaporradocrl

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // Outras configurações...
-  plugins: [react()], 
-
-  // ADICIONE ESTE BLOCO PARA TENTAR DESATIVAR O POSTCSS EXTERNO
+  // 1. CORREÇÃO DE CAMINHO PARA GITHUB PAGES
+  base: "./", 
+  
+  plugins: [react()],
+  
+  // 2. CORREÇÃO DE BUILD: Força o Vite a usar PostCSS de forma estável, 
+  // ignorando o erro de carregamento externo (desde que o postcss.config.cjs seja removido)
   css: {
-    postcss: null 
+    postcss: {}
   }
 })
